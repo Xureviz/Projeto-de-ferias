e0e6a9f80d884ce355607669f21447ce854b1648
Artur Carminati Serpa <ACSerpa@AD.MPSC.MP.BR>
2019-10-21 17:51:09 -0300

    Com db configurado

---
+++ models/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('orlando', "blaber", "123456", {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
